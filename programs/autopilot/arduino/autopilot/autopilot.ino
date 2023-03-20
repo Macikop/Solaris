25a79bad8c62d4987a65f34c9f5807693f59e66a
Maciej <maciej.piofczyk@gmail.com>
2023-03-20 19:46:25 +0100

    autopilot not yest

---
+++ programs/autopilot/arduino/autopilot/autopilot.ino
@@ -0,0 +1,15 @@
+#include <Servo.h>
+#include <i2c.h>
+#include <uart.h>
+#include <pio.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
